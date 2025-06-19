db010b4ea0e21f00ac0701929c6bc49843e2f4a8
Emon Halder <emon.maktech@gmail.com>
2025-06-19 15:21:02 +0600

    color added

---
+++ lib/utils/color/appColor.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xffC7AE6A);
+  static const Color secondaryColor = Color(0xffF8F6EE);
+  static const Color blackColor = Color(0xff1A1A1A);
+  static const Color greyColor = Color(0xff3D3D3D);
+  static const Color lightGrey = Color(0xff868686);
+}
