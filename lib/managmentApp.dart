fca180057bc3ebeb5d7014a21cfa59973777a07b
arafat593 <mdalarafat593@gmail.com>
2025-06-20 11:59:44 +0600

    add folder

--- lib/managmentApp.dart
+++ lib/managmentApp.dart
@@ -1,11 +1,26 @@
+import 'package:abyansf_asfmanagment_app/utils/themdata.dart';
 import 'package:abyansf_asfmanagment_app/view/auth/loginScreen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 
 class Managmentapp extends StatelessWidget {
   const Managmentapp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: LoginScreen());
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child){
+        return GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: themeData(),
+          home: const LoginScreen(),
+
+        );
+      },
+    );
   }
 }
