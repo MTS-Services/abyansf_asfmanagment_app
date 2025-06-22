8bf1280468e7f59cd160d00f473f3bb956ab887b
arafat593 <mdalarafat593@gmail.com>
2025-06-22 16:59:28 +0600

    update home

--- lib/managmentApp.dart
+++ lib/managmentApp.dart
@@ -1,4 +1,6 @@
 import 'package:abyansf_asfmanagment_app/utils/common/custom_bottom_bar.dart';
+import 'package:abyansf_asfmanagment_app/view/screens/main_screen/home_screen.dart';
+import 'package:abyansf_asfmanagment_app/view/screens/profile_pages/edit_profile.dart';
 import 'package:abyansf_asfmanagment_app/view/screens/profile_pages/profile_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -13,7 +15,7 @@ class ManagmentApp extends StatelessWidget {
       designSize: Size(360, 690),
       minTextAdapt: true,
       splitScreenMode: true,
-      builder: (context, child) => CustomBottomBar(),
+      builder: (context, child) => HomeScreen(),
     );
   }
 }
