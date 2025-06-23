b803a1b4efe9a45e4d1b06b61378e4a4240dc56a
arafat593 <mdalarafat593@gmail.com>
2025-06-23 08:57:16 +0600

    update booking history

--- lib/managmentApp.dart
+++ lib/managmentApp.dart
@@ -1,5 +1,6 @@
 import 'package:abyansf_asfmanagment_app/utils/common/custom_bottom_bar.dart';
 import 'package:abyansf_asfmanagment_app/view/screens/main_screen/home_screen.dart';
+import 'package:abyansf_asfmanagment_app/view/screens/profile_pages/booking_history.dart';
 import 'package:abyansf_asfmanagment_app/view/screens/profile_pages/edit_profile.dart';
 import 'package:abyansf_asfmanagment_app/view/screens/profile_pages/profile_screen.dart';
 import 'package:flutter/material.dart';
@@ -15,7 +16,7 @@ class ManagmentApp extends StatelessWidget {
       designSize: Size(360, 690),
       minTextAdapt: true,
       splitScreenMode: true,
-      builder: (context, child) => HomeScreen(),
+      builder: (context, child) => BookingHistory(),
     );
   }
 }
