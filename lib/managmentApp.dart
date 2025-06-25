01cb41507eb7a773f6a21c498e2fd847b749e751
remon <remonhowlader97@gmail.com>
2025-06-25 10:46:20 +0600

    event screen has been done

--- lib/managmentApp.dart
+++ lib/managmentApp.dart
@@ -1,7 +1,7 @@
-import 'package:abyansf_asfmanagment_app/view/screens/single_services_pages/single_beach_club_screen.dart';
+import 'package:abyansf_asfmanagment_app/utils/common/custom_bottom_bar.dart';
+import 'package:abyansf_asfmanagment_app/view/screens/main_screen/event_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 
 class ManagmentApp extends StatelessWidget {
   const ManagmentApp({super.key});
@@ -12,7 +12,7 @@ class ManagmentApp extends StatelessWidget {
       designSize: Size(360, 690),
       minTextAdapt: true,
       splitScreenMode: true,
-      builder: (context, child) => SingleBeachClubScreen()
+      builder: (context, child) => CustomBottomBar(),
     );
   }
 }
---
+++ lib/view/widget/day_time_row.dart
@@ -0,0 +1,30 @@
+import 'package:abyansf_asfmanagment_app/utils/style/appColor.dart';
+import 'package:abyansf_asfmanagment_app/utils/style/appStyle.dart';
+import 'package:flutter/material.dart';
+
+Widget dayTimeRow(String day, String time) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(vertical: 3),
+    child: Row(
+      children: [
+        Text(
+          day,
+          style: TextStyle(
+            fontSize: AppStyles.fontS,
+            fontWeight: AppStyles.weightRegular,
+            color: AppColors.blackColor,
+          ),
+        ),
+        Spacer(),
+        Text(
+          time,
+          style: TextStyle(
+            fontSize: AppStyles.fontS,
+            fontWeight: AppStyles.weightRegular,
+            color: AppColors.blackColor,
+          ),
+        ),
+      ],
+    ),
+  );
+}
